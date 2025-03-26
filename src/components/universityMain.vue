8cb1fcdd97f7d8834b3167f8e394f09aa87a6344
  Kanat Baibatyr <kanatbaibatyr@MacBook-Air-Kanat.local>
2025-03-27 00:48:41 +0500

    fix(hero component button)

--- src/components/universityMain.vue
+++ src/components/universityMain.vue
@@ -2,10 +2,10 @@
     <div class="absolute">
         <div class="flex flex-col items-center">
             <div class="flex justify-center gap-3 mb-4">
-                <h1 class="text-5xl text-white leading-20">Кокшетауский университет <br> имени Абая Мырзахметова</h1>
+                <h1 class="text-5xl text-white leading-12">Кокшетауский университет <br> имени Абая Мырзахметова</h1>
             </div>
-            <button class="bg-[#236A8D] flex items-center text-white px-6 py-6 rounded-md text-3xl">
-                <span>UPCOMING NOTICE</span> <span><img class="size-12" src='../assets/outline.svg' alt=""></span>
+            <button class="bg-[#236A8D] flex items-center text-white px-6 py-4 rounded-md text-xl">
+                <span>UPCOMING NOTICE</span> <span><img class="size-8" src='../assets/outline.svg' alt=""></span>
             </button>
         </div>
     </div>

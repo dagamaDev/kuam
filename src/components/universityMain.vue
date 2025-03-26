14c4ccd64d27869d7fe0fa24746baa5b56b0949e
  Kanat Baibatyr <kanatbaibatyr@MacBook-Air-Kanat.local>
2025-03-27 00:47:03 +0500

    fix(text size in main component file)

--- src/components/universityMain.vue
+++ src/components/universityMain.vue
@@ -2,7 +2,7 @@
     <div class="absolute">
         <div class="flex flex-col items-center">
             <div class="flex justify-center gap-3 mb-4">
-                <h1 class="text-7xl text-white leading-20">Кокшетауский университет <br> имени Абая Мырзахметова</h1>
+                <h1 class="text-5xl text-white leading-20">Кокшетауский университет <br> имени Абая Мырзахметова</h1>
             </div>
             <button class="bg-[#236A8D] flex items-center text-white px-6 py-6 rounded-md text-3xl">
                 <span>UPCOMING NOTICE</span> <span><img class="size-12" src='../assets/outline.svg' alt=""></span>

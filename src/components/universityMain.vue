52e9c0bdb64e57695a22563da7e3121dfd0893f8
  Kanat Baibatyr <kanatbaibatyr@MacBook-Air-Kanat.local>
2025-03-28 10:56:06 +0500

    fix(hero button)

--- src/components/universityMain.vue
+++ src/components/universityMain.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="absolute">
         <div class="flex flex-col items-center">
-            <div class="flex justify-center gap-3 mb-6">
+            <div class="flex justify-center gap-3 mb-10">
                 <h1 class="text-5xl text-white leading-12">Кокшетауский университет <br> имени Абая Мырзахметова</h1>
             </div>
-            <button class="bg-[#e6edf8] flex gap-2 items-center text-[#236a8d] px-6 py-4 rounded-md text-xl">
+            <button class="bg-[#e6edf8] flex gap-2 items-center text-[#236a8d] px-6 py-4 rounded-md text-lg">
                 <span>UPCOMING NOTICE</span> <span><img class="size-4" src='../assets/colored-vector.svg' alt=""></span>
             </button>
         </div>
